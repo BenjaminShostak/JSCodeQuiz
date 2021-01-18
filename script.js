0df0f7d4586993b6fa69131b2d4455c3888ad1a5
Benjamin Shostak <benshostak@rocketmail.com>
2021-01-17 23:39:23 -0800

    connected strt btn to Timer

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 var startTimer = document.querySelector("#startTimer");
+var timerElement = document.querySelector("#countdown");
+
+startTimer.addEventListener("click", function gameTimer() {
+    var timeLeft = 60;
+  console.log("startTimer");
+    var timeInterval = setInterval(function() {
+      timerElement.textContent = timeLeft + " seconds remaining";
+      timeLeft--;
+  
+      if (timeLeft === 0) {
+        timerElement.textContent = "";
+        clearInterval(timeInterval);
+      }
+  
+    }, 1000);
+  })
+
+
+  
